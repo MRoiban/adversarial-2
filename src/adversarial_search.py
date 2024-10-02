4699f9339cd9e3e411fb69e4fac29d0c464d1216
MRoiban <roiban.marius.alexandru@gmail.com>
2024-10-02 15:25:13 +0200

    Add display_world function and helper functions for minimax algorithm

--- src/adversarial_search.py
+++ src/adversarial_search.py
@@ -1,30 +1,79 @@
 from typing import Optional
 from competitive_env import CompetitiveEnv, S, A
+from competitive_world import CompetitiveWorld, CWorldState
 from math import inf
 from icecream import ic
+from copy import deepcopy
+import matplotlib.pyplot as plt
+from lle import World, Action
 
+DISPLAY = True
 
+def display_world(w: World):
+    if DISPLAY:
+        plt.imshow(w.get_image())
+        plt.axis("off")
+        plt.show()
 
+def getChildren(mdp: CompetitiveWorld, state:S):
+    children: list[CompetitiveWorld] = []
+    cState = CWorldState(
+            value=state.value,
+            current_agent=state.current_agent,
+            world_state=mdp.world.get_state(),
+        )
+    for action in mdp.available_actions(cState):
+        transitionWorld = CompetitiveWorld(mdp.world)
+        transitionState = transitionWorld.step(state, action)
+        transitionWorld.world.set_state(transitionState.world_state)
+        children.append((action, transitionWorld))
+        
+    return children
 
-def minimax(mdp: CompetitiveEnv[A, S], state: S, max_depth: int) -> Optional[A]:
-    """
-    1. When need to check which agent is playing: min or max
-    2.
-    """
-    if max_depth == 0 or mdp.is_final(state):
-        return 1 # heuristic
-    
+def closest_exit(mdp:CompetitiveWorld, state: S):
+    agent = mdp.world.agents_positions[state.current_agent]
+    best_distance = +inf
+    current_exit = None
+    distanceA = agent[0] + agent[1]
+    for ex in mdp.world.exit_pos:
+        distanceE = ex[0] + ex[1]; distance = abs(distanceE - distanceA)
+        if best_distance > distance:
+            best_distance = distance
+            current_exit = ex
+    return current_exit, best_distance
+
+def is_exit(ex, agent):
+    return 1 if ex == agent else 0
+
+def minimax(mdp: CompetitiveWorld, state: S, max_depth: int, depth=None) -> Optional[A]:
+    action = Action(4)
+    if depth == None:
+        depth = max_depth
+        
+    if depth == 0 or mdp.is_final(state):
+        ex = closest_exit(mdp, state)
+        agent = mdp.world.agents_positions[state.current_agent]
+        return [(ex[0][0] + ex[0][1]) + is_exit(ex[0], agent) - ex[1], action]
+
     if state.current_agent == 0:
         state.value = -inf
-        ic(mdp.available_actions(state))
-        for child in mdp.available_actions(state):
-            state.value = max(state.value, minimax(mdp, child, max_depth-1))
+        for action, child in getChildren(mdp, state):
+            state.current_agent = 1
+            maxi = minimax(child, state, max_depth, depth-1)
+            state.value = max(state.value, maxi[0])
+            if depth < max_depth:
+                return [state.value, action]
+            
     else:
         state.value = +inf
-        for child in mdp.available_actions(state):
-            state.value = min(state.value, minimax(mdp, child, max_depth-1))
+        for action, child in getChildren(mdp, state):
+            state.current_agent = 1
+            mini = minimax(child, state, max_depth, depth-1)
+            state.value = min(state.value, mini[0])
+            if depth < max_depth:
+                return [state.value, action]
     
-    return state.value
+    return action
 
 
 def alpha_beta(mdp: CompetitiveEnv[A, S], state: S, max_depth: int) -> Optional[A]:
