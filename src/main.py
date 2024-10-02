18b7eaf96fb7d2d1247801a9cc6ba54dca2cbf71
MRoiban <roiban.marius.alexandru@gmail.com>
2024-10-02 15:25:18 +0200

    Update main.py with CompetitiveWorld and minimax algorithm

--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
-from lle import World
+from lle import World, Action
 from competitive_world import CompetitiveWorld
+
 import random
 import adversarial_search
 
 
 def main():
     """Vous pouvez tester et débugger votre programme ici"""
-    world = CompetitiveWorld(World("S0 X"))
-    state = world.reset()
-    adversarial_search.minimax(world, state, 1)
+    world = CompetitiveWorld(
+        World(
+            """
+        .  L1W
+        S0 S1
+        X  X
+        """
+        )
+    )
+    # state = world.reset()
+    # transitionState = world.step(state, Action(1))
+    # world.world.set_state(transitionState.world_state)
+    print(adversarial_search.minimax(world, world.reset(),4))
 
 
 if __name__ == "__main__":
